ab68b93f25747465b298a80b067f346a8dcf1e99
valentin <valenzo26@msn.com>
2023-12-21 23:38:14 +0100

    cull date not render on screen

--- src/Timeline/Timeline.js
+++ src/Timeline/Timeline.js
@@ -5,7 +5,7 @@ import './timeline.css';
 const now = new Date(Date.now());
 
 // const BIG_BANG_YEAR = -13.8 * 1000000000;
-const BIG_BANG_YEAR = 2000;
+const BIG_BANG_YEAR = 2020;
 
 export class TimelineDate {
   constructor(year, month = 0, day = 1) {
@@ -82,7 +82,86 @@ export class TimelineDate {
     return result;
   }
 
-  add(dayCount) {}
+  clone() {
+    return new TimelineDate(this.year, this.month, this.day);
+  }
+
+  add(dayCount) {
+    if (!dayCount) return this; // no days to add
+    /**
+     *
+     * @returns {boolean} true meaning there are still days to add
+     */
+    const fillMonth = () => {
+      // fill month
+      const dayMissingInMonth =
+        TimelineDate.monthDayCount(this.year, this.month) - this.day;
+      if (dayMissingInMonth) {
+        if (dayCount <= dayMissingInMonth) {
+          // filling this month and done
+          this.day += dayCount;
+          return false; // add finished
+        } else {
+          // add these missing days
+          this.day += dayMissingInMonth;
+          dayCount -= dayMissingInMonth; // > 0 with the previous condition
+        }
+      }
+      // month filled and still days to add
+      if (this.month < 11) {
+        // no december
+        this.month++;
+      } else {
+        this.month = 0;
+        this.year++;
+      }
+      this.day = 1;
+      dayCount--;
+
+      return dayCount > 0; // they are still days to add
+    };
+    if (!fillMonth()) return this; // no more days
+
+    // there are still days to add
+
+    // fill this.year
+    for (let month = this.month; month < 12; month++) {
+      // fill month
+      if (!fillMonth()) return this;
+    }
+
+    // year filled and still days to add
+    if (this.month != 0 || this.day != 1 || dayCount <= 0)
+      throw new Error('year not filled properly');
+
+    // add as many year as possible
+    let currentYearDayCount = TimelineDate.yearDayCount(this.year);
+    while (dayCount >= currentYearDayCount) {
+      dayCount -= currentYearDayCount;
+      this.year++;
+      currentYearDayCount = TimelineDate.yearDayCount(this.year);
+    }
+
+    if (dayCount == 0) return this; // no more days
+
+    // year filled and still days to add but less than the current year day count
+    if (this.month != 0 || this.day != 1 || dayCount >= currentYearDayCount)
+      throw new Error('year not filled properly');
+
+    // fill this.year
+    for (let month = 0; month < 12; month++) {
+      // fill month
+      if (!fillMonth()) return this;
+    }
+
+    throw new Error('fill month above should return at a moment');
+  }
+
+  toString() {
+    return (
+      this.day + ', ' + TimelineDate.monthToString(this.month) + ' ' + this.year
+    );
+  }
 
   static assertYear(year) {
     return year <= parseInt(now.getFullYear()) && year >= BIG_BANG_YEAR;
@@ -109,9 +188,13 @@ export class TimelineDate {
     );
   }
 
+  // ref: https://www.epochconverter.com/years
+  // Leap years: Every year that is divisible by four is a leap year, except for years that are divisible by 100, but not by 400.
   static yearIsLeap(year) {
-    // 1992 is leap
-    return !Math.abs(1992 - year) % 4;
+    if (year % 100 == 0 && year % 400 != 0) {
+      return false;
+    }
+    return year % 4 == 0;
   }
 
   static monthToString(month) {
@@ -146,11 +229,11 @@ export class TimelineDate {
   }
 
   static yearDayCount(year) {
-    let result = 0;
-    for (let i = 0; i < 12; i++) {
-      result += TimelineDate.monthDayCount(year, i);
+    if (TimelineDate.yearIsLeap(year)) {
+      return 366;
+    } else {
+      return 365;
     }
-    return result;
   }
 
   static monthDayCount(year, month) {
@@ -217,8 +300,8 @@ export class Timeline extends HTMLDivElement {
     if (this.translation == null) this.translation = 0;
 
     //DEBUG
-    // this.scale = 1;
-    // this.translation = 0;
+    this.scale = 1;
+    this.translation = 0;
 
     this.canvas.addEventListener('wheel', (event) => {
       const worldX = (event.clientX - this.translation) / this.scale;
@@ -268,12 +351,21 @@ export class Timeline extends HTMLDivElement {
     ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
     // compute min and max date on screen
-    const totalDayMinDate = -Math.floor(this.translation / this.dayWidth);
-    console.log(totalDayMinDate);
+    const minDateOnScreen = this.minDate
+      .clone()
+      .add(Math.floor(-this.translation / this.dayWidth));
+    const maxDateOnScreen = minDateOnScreen
+      .clone()
+      .add(Math.ceil(window.innerWidth / this.dayWidth));
+    console.log(
+      this.translation,
+      minDateOnScreen.toString(),
+      maxDateOnScreen.toString()
+    );
 
     {
       // day rendering TODO make this generic
-      if (this.dayWidth >= 20) {
+      if (this.dayWidth >= 20 || true) {
         // timeline day/month/year
         const dayHeight = this.canvas.height / 3;
         const monthHeight = (this.canvas.height * 2) / 3;
@@ -296,6 +388,14 @@ export class Timeline extends HTMLDivElement {
         };
 
         for (let year = this.minDate.year; year <= this.maxDate.year; year++) {
+          if (year < minDateOnScreen.year) {
+            cursor += this.dayWidth * TimelineDate.yearDayCount(year);
+            continue;
+          }
+          if (year > maxDateOnScreen.year) {
+            break;
+          }
+
           ctx.font = computeFont(
             year,
             this.dayWidth * TimelineDate.yearDayCount(year),
@@ -315,6 +415,14 @@ export class Timeline extends HTMLDivElement {
 
           // ctx.fillText(year, cursor + this.translation, yearHeight);
           for (let month = 0; month < 12; month++) {
+            if (year == minDateOnScreen.year && month < minDateOnScreen.month) {
+              cursor += TimelineDate.monthDayCount(year, month) * this.dayWidth;
+              continue;
+            }
+
+            if (year == maxDateOnScreen.year && month > maxDateOnScreen.month)
+              break;
+
             ctx.font = computeFont(
               TimelineDate.monthToString(month),
               this.dayWidth * TimelineDate.monthDayCount(year, month),
@@ -335,6 +443,22 @@ export class Timeline extends HTMLDivElement {
 
             const monthDayCount = TimelineDate.monthDayCount(year, month);
             for (let day = 1; day <= monthDayCount; day++) {
+              if (
+                year == minDateOnScreen.year &&
+                month == minDateOnScreen.month &&
+                day < minDateOnScreen.day
+              ) {
+                cursor += this.dayWidth;
+                continue;
+              }
+
+              if (
+                year == maxDateOnScreen.year &&
+                month == maxDateOnScreen.month &&
+                day > maxDateOnScreen.day
+              )
+                break;
+
               ctx.font = this.dayWidth * 0.5 + "px 'Segoe UI'";
 
               const size =
@@ -371,9 +495,11 @@ export class Timeline extends HTMLDivElement {
   }
 
   set translation(value) {
-    this._translation = Math.max(
-      -(this.totalDays * this.dayWidth - window.innerWidth), // TODO remove window.innerWidth
-      Math.min(value, 0)
+    this._translation = Math.round(
+      Math.max(
+        -(this.totalDays * this.dayWidth - window.innerWidth), // TODO remove window.innerWidth
+        Math.min(value, 0)
+      )
     );
   }
 
